feb4c58288268444a2e4b475019a90482df55634
Xtrendence <xtrendence@gmail.com>
2021-10-21 17:40:16 +0100

    Started implementing API login functionality.

--- api/src/server.ts
+++ api/src/server.ts
@@ -14,9 +14,14 @@ const schema = buildSchema(utils.getSchema());
 const app: express.Application = express();
 const port: number = 1999;
 
-const db = new DB().initialize();
+const db = new DB();
+db.initialize();
+
+utils.db = db.db;
 
 app.use(cors());
+app.use(express.urlencoded());
+app.use(express.json());
 
 app.use("/graphql", graphqlHTTP({ 
 	schema: schema,
@@ -31,8 +36,10 @@ app.listen(port, () => {
 	console.log(`GraphQL API Listening At http://localhost:${port}/graphql`)
 });
 
-app.post("/login", (request, response) => {
-
+app.post("/login", async (request, response) => {
+	let username = request.body.username;
+	let password = request.body.password;
+	response.send(await utils.login(username, password));
 });
 
 console.log("Starting Server... ", new Date().toTimeString().split(" ")[0]);
--- api/src/utils/Utils.ts
+++ api/src/utils/Utils.ts
@@ -1,7 +1,10 @@
 import fs, { existsSync, mkdirSync, writeFileSync, readFileSync } from "fs";
 import path from "path";
+import bcrypt from "bcrypt";
+import sqlite3, { Database } from "@louislam/sqlite3";
 
 export default class Utils {
+	db: sqlite3.Database | undefined;
 	dataFolder: string;
 
 	constructor() {
@@ -13,6 +16,33 @@ export default class Utils {
 			return true;
 		}
 		return false;
+	}
+
+	async login(username: string, password: string) {
+		return new Promise(async (resolve, reject) => {
+			this.db?.get("SELECT * FROM User WHERE username = ?", [username], async (error, row) => {
+				if(error) {
+					console.log(error);
+				} else {
+					if(row === undefined) {
+						reject("!User Not Found!");
+						return;
+					}
+
+					let valid = await bcrypt.compare(password, row.password);
+
+					if(valid) {
+
+					} else {
+						reject("!Incorrect Password!");
+					}
+				}
+			});
+		});
+	}
+
+	generateToken() {
+		
 	}
 
 	checkFiles() {
