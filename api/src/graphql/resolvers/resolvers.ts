d14921b20ab428f40840392cee893e493ba8a652
Xtrendence <xtrendence@gmail.com>
2021-10-22 01:02:46 +0100

    Added "Stock" resolvers.

--- api/src/graphql/resolvers/resolvers.ts
+++ api/src/graphql/resolvers/resolvers.ts
@@ -1,9 +1,11 @@
 import { createUser, readUser, updateUser, deleteUser } from "./user";
 import { createCoin, readCoinByID, readCoinBySymbol, updateCoin, deleteCoin } from "./coin";
+import { createStock, readStockByID, readStockBySymbol, updateStock, deleteStock } from "./stock";
 
 let resolvers = {
 	createUser, readUser, updateUser, deleteUser,
-	createCoin, readCoinByID, readCoinBySymbol, updateCoin, deleteCoin
+	createCoin, readCoinByID, readCoinBySymbol, updateCoin, deleteCoin,
+	createStock, readStockByID, readStockBySymbol, updateStock, deleteStock
 };
 
 export default resolvers;
---
+++ api/src/graphql/resolvers/stock.ts
@@ -0,0 +1,91 @@
+import Utils from "../../utils/Utils";
+import DB from "../../utils/DB";
+import Stock from "../../models/Stock";
+
+const utils = new Utils();
+const db = new DB();
+
+export async function createStock({ token, userID, assetID, assetSymbol }: any) {
+	let valid = await utils.verifyToken(userID, token);
+
+	if(valid) {
+		db.runQuery("INSERT INTO Stock (assetID, assetSymbol) VALUES (?, ?)", [assetID, assetSymbol]);
+		return "Done";
+	} else {
+		return "Unauthorized";
+	}
+}
+
+export async function readStockByID({ token, userID, assetID }: any) {
+	return new Promise(async (resolve, reject) => {
+		let valid = await utils.verifyToken(userID, token);
+
+		if(valid) {
+			db.db?.get("SELECT * FROM Stock WHERE assetID = ?", [assetID], (error, row) => {
+				if(error) {
+					console.log(error);
+					reject();
+				} else {
+					if(row === undefined) {
+						reject("!Stock Not Found!");
+						return;
+					}
+
+					let stock = new Stock(assetID, row.assetSymbol);
+					stock.stockID = row.stockID;
+					resolve(stock);
+				}
+			});
+		} else {
+			reject("!Unauthorized!");
+		}
+	});
+}
+
+export async function readStockBySymbol({ token, userID, assetSymbol }: any) {
+	return new Promise(async (resolve, reject) => {
+		let valid = await utils.verifyToken(userID, token);
+
+		if(valid) {
+			db.db?.get("SELECT * FROM Stock WHERE assetSymbol = ?", [assetSymbol], (error, row) => {
+				if(error) {
+					console.log(error);
+					reject();
+				} else {
+					if(row === undefined) {
+						reject("!Stock Not Found!");
+						return;
+					}
+
+					let stock = new Stock(row.assetID, assetSymbol);
+					stock.stockID = row.stockID;
+					resolve(stock);
+				}
+			});
+		} else {
+			reject("!Unauthorized!");
+		}
+	});
+}
+
+export async function updateStock({ token, userID, stockID, assetID, assetSymbol }: any) {
+	let valid = await utils.verifyToken(userID, token);
+
+	if(valid) {
+		db.runQuery("UPDATE Stock SET assetID = ?, assetSymbol = ? WHERE stockID = ?", [assetID, assetSymbol, stockID]);
+		return "Done";
+	} else {
+		return "Unauthorized";
+	}
+}
+
+export async function deleteStock({ token, userID, stockID }: any) {
+	let valid = await utils.verifyToken(userID, token);
+
+	if(valid) {
+		db.runQuery("DELETE FROM Stock WHERE stockID = ?", [stockID]);
+		return "Done";
+	} else {
+		return "Unauthorized";
+	}
+}
