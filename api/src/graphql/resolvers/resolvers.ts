4e1b43d0c592aaae624b3e272975c5c64f264e52
Xtrendence <xtrendence@gmail.com>
2021-10-23 17:41:34 +0100

    Added "Watchlist" resolvers.

--- api/src/graphql/resolvers/resolvers.ts
+++ api/src/graphql/resolvers/resolvers.ts
@@ -1,11 +1,13 @@
 import { createUser, readUser, updateUser, deleteUser } from "./user";
 import { createSetting, readSetting, updateSetting, deleteSetting } from "./setting";
+import { createWatchlist, readWatchlist, updateWatchlist, deleteWatchlist } from "./watchlist";
 import { createCoin, readCoinByID, readCoinBySymbol, updateCoin, deleteCoin } from "./coin";
 import { createStock, readStockByID, readStockBySymbol, updateStock, deleteStock } from "./stock";
 
 let resolvers = {
 	createUser, readUser, updateUser, deleteUser,
 	createSetting, readSetting, updateSetting, deleteSetting,
+	createWatchlist, readWatchlist, updateWatchlist, deleteWatchlist,
 	createCoin, readCoinByID, readCoinBySymbol, updateCoin, deleteCoin,
 	createStock, readStockByID, readStockBySymbol, updateStock, deleteStock
 };
---
+++ api/src/graphql/resolvers/watchlist.ts
@@ -0,0 +1,65 @@
+import Utils from "../../utils/Utils";
+import DB from "../../utils/DB";
+import Watchlist from "../../models/Watchlist";
+
+const utils = new Utils();
+const db = new DB();
+
+export async function createWatchlist({ token, userID, assetID, assetSymbol, assetType }: any) {
+	let valid = await utils.verifyToken(userID, token);
+
+	if(valid) {
+		db.runQuery("INSERT INTO Watchlist (userID, assetID, assetSymbol, assetType) VALUES (?, ?, ?, ?)", [userID, assetID, assetSymbol, assetType]);
+		return "Done";
+	} else {
+		return "Unauthorized";
+	}
+}
+
+export async function readWatchlist({ token, userID }: any) {
+	return new Promise(async (resolve, reject) => {
+		let valid = await utils.verifyToken(userID, token);
+
+		if(valid) {
+			db.db?.get("SELECT * FROM Watchlist WHERE userID = ?", [userID], (error, row) => {
+				if(error) {
+					console.log(error);
+					reject();
+				} else {
+					if(row === undefined) {
+						reject("!Watchlist Not Found!");
+						return;
+					}
+
+					let watchlist = new Watchlist(userID, row.assetID, row.assetSymbol, row.assetType);
+					watchlist.watchlistID = row.watchlistID;
+					resolve(watchlist);
+				}
+			});
+		} else {
+			reject("!Unauthorized!");
+		}
+	});
+}
+
+export async function updateWatchlist({ token, userID, watchlistID, assetID, assetSymbol, assetType }: any) {
+	let valid = await utils.verifyToken(userID, token);
+
+	if(valid) {
+		db.runQuery("UPDATE Watchlist SET assetID = ?, assetSymbol = ?, assetType = ? WHERE watchlistID = ?", [assetID, assetSymbol, assetType, watchlistID]);
+		return "Done";
+	} else {
+		return "Unauthorized";
+	}
+}
+
+export async function deleteWatchlist({ token, userID, watchlistID }: any) {
+	let valid = await utils.verifyToken(userID, token);
+
+	if(valid) {
+		db.runQuery("DELETE FROM Watchlist WHERE watchlistID = ?", [watchlistID]);
+		return "Done";
+	} else {
+		return "Unauthorized";
+	}
+}
