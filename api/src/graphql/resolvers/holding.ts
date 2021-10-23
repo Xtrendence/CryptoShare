72952eecdb4205976218d8dcb1cc0c73eb1ee674
Xtrendence <xtrendence@gmail.com>
2021-10-23 18:01:43 +0100

    Added "Holding" resolvers.

---
+++ api/src/graphql/resolvers/holding.ts
@@ -0,0 +1,65 @@
+import Utils from "../../utils/Utils";
+import DB from "../../utils/DB";
+import Holding from "../../models/Holding";
+
+const utils = new Utils();
+const db = new DB();
+
+export async function createHolding({ token, userID, holdingAssetID, holdingAssetSymbol, holdingAssetAmount, holdingAssetType }: any) {
+	let valid = await utils.verifyToken(userID, token);
+
+	if(valid) {
+		db.runQuery("INSERT INTO Holding (userID, holdingAssetID, holdingAssetSymbol, holdingAssetAmount, holdingAssetType) VALUES (?, ?, ?, ?, ?)", [userID, holdingAssetID, holdingAssetSymbol, holdingAssetAmount, holdingAssetType]);
+		return "Done";
+	} else {
+		return "Unauthorized";
+	}
+}
+
+export async function readHolding({ token, userID }: any) {
+	return new Promise(async (resolve, reject) => {
+		let valid = await utils.verifyToken(userID, token);
+
+		if(valid) {
+			db.db?.get("SELECT * FROM Holding WHERE userID = ?", [userID], (error, row) => {
+				if(error) {
+					console.log(error);
+					reject();
+				} else {
+					if(row === undefined) {
+						reject("!Holding Not Found!");
+						return;
+					}
+
+					let holding = new Holding(userID, row.holdingAssetID, row.holdingAssetSymbol, row.holdingAssetAmount, row.holdingAssetType);
+					holding.holdingID = row.holdingID;
+					resolve(holding);
+				}
+			});
+		} else {
+			reject("!Unauthorized!");
+		}
+	});
+}
+
+export async function updateHolding({ token, userID, holdingID, holdingAssetID, holdingAssetSymbol, holdingAssetAmount, holdingAssetType }: any) {
+	let valid = await utils.verifyToken(userID, token);
+
+	if(valid) {
+		db.runQuery("UPDATE Holding SET holdingAssetID = ?, holdingAssetSymbol = ?, holdingAssetAmount = ?, holdingAssetType = ? WHERE holdingID = ? AND userID = ?", [holdingAssetID, holdingAssetSymbol, holdingAssetAmount, holdingAssetType, holdingID, userID]);
+		return "Done";
+	} else {
+		return "Unauthorized";
+	}
+}
+
+export async function deleteHolding({ token, userID, holdingID }: any) {
+	let valid = await utils.verifyToken(userID, token);
+
+	if(valid) {
+		db.runQuery("DELETE FROM Holding WHERE holdingID = ? AND userID = ?", [holdingID, userID]);
+		return "Done";
+	} else {
+		return "Unauthorized";
+	}
+}
--- api/src/graphql/resolvers/resolvers.ts
+++ api/src/graphql/resolvers/resolvers.ts
@@ -1,5 +1,6 @@
 import { createUser, readUser, updateUser, deleteUser } from "./user";
 import { createSetting, readSetting, updateSetting, deleteSetting } from "./setting";
+import { createHolding, readHolding, updateHolding, deleteHolding } from "./holding";
 import { createWatchlist, readWatchlist, updateWatchlist, deleteWatchlist } from "./watchlist";
 import { createCoin, readCoinByID, readCoinBySymbol, updateCoin, deleteCoin } from "./coin";
 import { createStock, readStockByID, readStockBySymbol, updateStock, deleteStock } from "./stock";
@@ -7,6 +8,7 @@ import { createStock, readStockByID, readStockBySymbol, updateStock, deleteStock
 let resolvers = {
 	createUser, readUser, updateUser, deleteUser,
 	createSetting, readSetting, updateSetting, deleteSetting,
+	createHolding, readHolding, updateHolding, deleteHolding,
 	createWatchlist, readWatchlist, updateWatchlist, deleteWatchlist,
 	createCoin, readCoinByID, readCoinBySymbol, updateCoin, deleteCoin,
 	createStock, readStockByID, readStockBySymbol, updateStock, deleteStock
