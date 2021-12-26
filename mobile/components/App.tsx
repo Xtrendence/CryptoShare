b233b49371435a1d241a7d9159c042a76e9b8cca
Xtrendence <xtrendence@gmail.com>
2021-12-26 02:21:56 +0000

    Set up mobile app directory structure.

---
+++ mobile/components/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+function App() {
+	return (
+		<View>
+			<Text>CryptoShare</Text>
+		</View>
+	);
+}
+
+export default App;
