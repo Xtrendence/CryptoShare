e6ce2984fb44529a4851cf16d7603f55483de036
Xtrendence <xtrendence@gmail.com>
2022-01-19 00:58:33 +0000

    Added unit test for bot server status.

--- api/tests/api.test.ts
+++ api/tests/api.test.ts
@@ -1,8 +1,10 @@
 
 import API from "../src/utils/API";
+import { io, Socket } from "socket.io-client";
 import utils from "./utils";
 
 let api: API;
+let socket: Socket;
 
 describe("API Testing", () => {
 	beforeAll(async () => {
@@ -12,12 +14,21 @@ describe("API Testing", () => {
 
 	afterAll(async () => {
 		await api?.kill();
+		socket.close();
 	});
 
 	describe("Server Tests", () => {
-		test("Ensure the server is running.", async () => {
+		test("Ensure the API server is running.", async () => {
 			let response = await utils.request("GET", `http://localhost:${api.portAPI}/status`, null);
 			expect(response).toEqual({ status:"online" });
+		});
+
+		test("Ensure the bot server is running.", (done) => {
+			socket = io(`http://localhost:${api.portBot}`);
+
+			socket.io.on("open", () => {
+				done();
+			});
 		});
 	});
 });
