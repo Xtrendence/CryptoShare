aa8eb7dd64fb5f1835ab087669aa9ed7cf126cce
Xtrendence <xtrendence@gmail.com>
2022-01-29 10:44:44 +0000

    Fixed issue #18.

--- api/src/graphql/resolvers/coin.ts
+++ api/src/graphql/resolvers/coin.ts
@@ -17,17 +17,22 @@ export async function readCoin({ token, userID, assetID, assetSymbol, currency }
 					reject();
 				} else {
 					if(row === undefined || !Utils.validJSON(row.data) || Utils.refetchRequired(JSON.parse(row.data).time)) {
-						let from = Math.floor(Utils.previousYear(new Date()).getTime() / 1000);
-						let now = Math.floor(new Date().getTime() / 1000);
+						try {
+							let from = Math.floor(Utils.previousYear(new Date()).getTime() / 1000);
+							let now = Math.floor(new Date().getTime() / 1000);
 
-						let historicalData = await Utils.request("GET", "https://api.coingecko.com/api/v3/coins/" + assetID + "/market_chart/range?vs_currency=" + currency + "&from=" + from + "&to=" + now, null);
+							let historicalData = await Utils.request("GET", "https://api.coingecko.com/api/v3/coins/" + assetID + "/market_chart/range?vs_currency=" + currency + "&from=" + from + "&to=" + now, null);
 
-						let data = JSON.stringify({ time:now, historicalData:historicalData });
+							let data = JSON.stringify({ time:now, historicalData:historicalData });
 
-						db.runQuery("INSERT INTO Coin (assetID, assetSymbol, data) VALUES (?, ?, ?)", [id, assetSymbol, data]);
+							db.runQuery("INSERT INTO Coin (assetID, assetSymbol, data) VALUES (?, ?, ?)", [id, assetSymbol, data]);
 
-						let coin = new Coin(id, assetSymbol, data);
-						resolve(coin);
+							let coin = new Coin(id, assetSymbol, data);
+							resolve(coin);
+						} catch(error) {
+							console.log(error);
+							reject(error);
+						}
 					} else {
 						let coin = new Coin(id, row.assetSymbol, row.data);
 						resolve(coin);
