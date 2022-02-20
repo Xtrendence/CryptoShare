e008f4528834a770f969d3d799597a18c8ef0886
Xtrendence <xtrendence@gmail.com>
2022-02-20 13:04:29 +0000

    Added a delete button to watchlist rows.

--- web/assets/js/functions/dashboard.js
+++ web/assets/js/functions/dashboard.js
@@ -813,9 +813,13 @@ function createWatchlistListRows(marketCryptoData, marketStocksData, watchlistDa
 								<span class="price-change">24h Change: ${priceChangeDay}%</span>
 							</div>
 						</div>
+						<button class="action-button delete">
+							<svg class="delete" width="1792" height="1792" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path class="delete" d="M704 1376v-704q0-14-9-23t-23-9h-64q-14 0-23 9t-9 23v704q0 14 9 23t23 9h64q14 0 23-9t9-23zm256 0v-704q0-14-9-23t-23-9h-64q-14 0-23 9t-9 23v704q0 14 9 23t23 9h64q14 0 23-9t9-23zm256 0v-704q0-14-9-23t-23-9h-64q-14 0-23 9t-9 23v704q0 14 9 23t23 9h64q14 0 23-9t9-23zm-544-992h448l-48-117q-7-9-17-11h-317q-10 2-17 11zm928 32v64q0 14-9 23t-23 9h-96v948q0 83-47 143.5t-113 60.5h-832q-66 0-113-58.5t-47-141.5v-952h-96q-14 0-23-9t-9-23v-64q0-14 9-23t23-9h309l70-167q15-37 54-63t79-26h320q40 0 79 26t54 63l70 167h309q14 0 23 9t9 23z"/></svg>
+						</button>
 					</div>
 				`;
 
+				addWatchlistDeleteEvent(div, asset);
 				addWatchlistRowEvent(div, asset);
 
 				rows.push(div);
@@ -851,9 +855,13 @@ function createWatchlistListRows(marketCryptoData, marketStocksData, watchlistDa
 								<span class="price-change">24h Change: ${priceChangeDay}%</span>
 							</div>
 						</div>
+						<button class="action-button delete">
+							<svg class="delete" width="1792" height="1792" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path class="delete" d="M704 1376v-704q0-14-9-23t-23-9h-64q-14 0-23 9t-9 23v704q0 14 9 23t23 9h64q14 0 23-9t9-23zm256 0v-704q0-14-9-23t-23-9h-64q-14 0-23 9t-9 23v704q0 14 9 23t23 9h64q14 0 23-9t9-23zm256 0v-704q0-14-9-23t-23-9h-64q-14 0-23 9t-9 23v704q0 14 9 23t23 9h64q14 0 23-9t9-23zm-544-992h448l-48-117q-7-9-17-11h-317q-10 2-17 11zm928 32v64q0 14-9 23t-23 9h-96v948q0 83-47 143.5t-113 60.5h-832q-66 0-113-58.5t-47-141.5v-952h-96q-14 0-23-9t-9-23v-64q0-14 9-23t23-9h309l70-167q15-37 54-63t79-26h320q40 0 79 26t54 63l70 167h309q14 0 23 9t9 23z"/></svg>
+						</button>
 					</div>
 				`;
 
+				addWatchlistDeleteEvent(div, asset);
 				addWatchlistRowEvent(div, asset);
 
 				rows.push(div);
@@ -866,16 +874,8 @@ function createWatchlistListRows(marketCryptoData, marketStocksData, watchlistDa
 	return rows;
 }
 
-function addWatchlistRowEvent(div, asset) {
-	div.addEventListener("click", () => {
-		if(div.parentElement.id === "market-list-stocks") {
-			buttonMarketSearch.click();
-			document.getElementById("popup-input-search").value = asset.assetSymbol;
-			document.getElementById("popup-choice-stock").click();
-			document.getElementById("popup-input-search").parentElement.getElementsByClassName("button-confirm")[0].click();
-			return;
-		}
-
+function addWatchlistDeleteEvent(div, asset) {
+	div.getElementsByClassName("action-button delete")[0].addEventListener("click", () => {
 		let userID = localStorage.getItem("userID");
 		let token = localStorage.getItem("token");
 
@@ -899,6 +899,44 @@ function addWatchlistRowEvent(div, asset) {
 				errorNotification("Couldn't delete asset.");
 			}
 		});
+	});
+}
+
+function addWatchlistRowEvent(div, asset) {
+	div.addEventListener("click", async (event) => {
+		if(!event.target.classList.contains("delete")) {
+			showLoading(5000, "Loading...");
+			
+			let currency = getCurrency();
+
+			if(asset.assetType === "crypto") {
+				let data = await cryptoAPI.getMarketByID(currency, asset.assetID);
+				let info = parseCryptoMarketData(currency, data[0]);
+				showCryptoMarketData(info);
+			} else {
+				let resultPrice = await fetchStockPrice(currency, [asset.assetSymbol], true);
+
+				if("error" in resultPrice) {
+					errorNotification(resultPrice.error);
+					return;
+				}
+
+				let infoPrice = resultPrice[Object.keys(resultPrice)[0]].priceData;
+				infoPrice.currency = currency;
+
+				let resultHistorical = await fetchStockHistorical(currency, asset.assetSymbol, true);
+
+				if("error" in resultHistorical) {
+					errorNotification(resultHistorical.error);
+					return;
+				}
+
+				let infoHistorical = resultHistorical.data.historicalData.chart.result[0];
+				infoHistorical.currency = currency;
+
+				showStockMarketData(infoPrice, infoHistorical);
+			}
+		}
 	});
 }
 
