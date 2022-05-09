ea8e7517f34d0bb40b6f6b4fd0f2a227d284212f
Xtrendence <xtrendence@gmail.com>
2022-05-09 01:47:41 +0100

    Changed "bcrypt" library.

--- api/src/graphql/resolvers/user.ts
+++ api/src/graphql/resolvers/user.ts
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import User from "../../models/User";
 import DB from "../../utils/DB";
 import Utils from "../../utils/Utils";
--- api/src/utils/Utils.ts
+++ api/src/utils/Utils.ts
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import crypto from "crypto";
 import { existsSync, mkdirSync, writeFileSync, readFileSync } from "fs";
 // @ts-ignore
